6247fcc37be53f28c8a0f5d2fd50d770076a2e5c
Cavliuc <59823424+cavliuc@users.noreply.github.com>
2022-08-03 20:48:01 +0200

    add active class for navigation item

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const header = document.getElementById("header__content");
+const navigation = document.getElementById("navigation");
+const btns = navigation.getElementsByClassName("navigation__item");
 
 function toggleClass() {
-  header.classList.toggle("active")
+  header.classList.toggle("active");
+}
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    const current = document.getElementsByClassName("item-active");
+    current[0].className = current[0].className.replace(" item-active", "");
+    this.className += " item-active";
+  });
 }
